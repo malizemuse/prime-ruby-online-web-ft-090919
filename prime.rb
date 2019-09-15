60bcc173cbe7b8bad9275b68f6d0eecfc268fe39
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 17:16:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,11 +6,9 @@ def prime?(num)
     
   else
     
-    (2...num).to_a.each do |test|
-      
+    (2...num).to_a.each { |test|
       return false if num % test == 0 
-      
-    end 
+    }
     
     return true
     
