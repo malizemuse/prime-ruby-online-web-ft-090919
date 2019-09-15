dfa9596a73128fe3bdaa37d52210c6887e115ac5
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 17:16:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  
+  if num <= 1 
+    false
+    
+  else
+    
+    (2...num).to_a.each { |test|
+      return false if num % test == 0 
+    }
+    
+    return true
+    
+  end 
+  
+end
